1c262eb2f9998eca219988f9555bc162529807c1
Ashkan Bahmani <74037732+ashkanbmn@users.noreply.github.com>
2023-12-05 14:21:37 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+ 
 import 'package:credit_card_ml/splash_screen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -7,21 +8,26 @@ import 'package:ml_card_scanner/ml_card_scanner.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
 
-  runApp(Directionality(
-      textDirection: TextDirection.rtl,
-      child: MaterialApp(
-        title: "AI Credit Card Scanner",
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            useMaterial3: true,
-            pageTransitionsTheme: const PageTransitionsTheme(
-              builders: {
-                TargetPlatform.android: CupertinoPageTransitionsBuilder(),
-              },
-            ),
-          ),
-          home: const SplashScreen())));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "AI Credit Card Scanner",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        pageTransitionsTheme: const PageTransitionsTheme(
+          builders: {
+            TargetPlatform.android: CupertinoPageTransitionsBuilder(),
+          },
+        ),
+      ),
+      home: const SplashScreen(),
+    );
+  }
 }
 
 class MainScreen extends StatefulWidget {
@@ -37,18 +43,14 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   void initState() {
+    super.initState();
     _controller
-      ..setCardListener((value) {
-        setState(() {
-          cardInfo = value;
-        });
-      })
+      ..setCardListener((value) => setState(() => cardInfo = value))
       ..setErrorListener((exception) {
         if (kDebugMode) {
           print('خطا: ${exception.message}');
         }
       });
-    super.initState();
   }
 
   void copyCardInfoToClipboard() {
@@ -62,99 +64,128 @@ class _MainScreenState extends State<MainScreen> {
     }
   }
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        surfaceTintColor: Theme.of(context).scaffoldBackgroundColor,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        title: const Text('اسکن کارت اعتباری با هوش مصنوعی'),
-        centerTitle: true,
-      ),
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          ScannerWidget(
-            oneShotScanning: true,
-            cameraResolution: CameraResolution.high,
-            controller: _controller,
-            overlayOrientation: CardOrientation.landscape,
-            overlayText: const Center(
-                child: Text(
-              "کارت بانکی را اسکن کنید",
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            )),
-          ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: ClipRRect(
-              borderRadius: const BorderRadius.all(Radius.circular(24)),
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                height: MediaQuery.of(context).size.height / 7,
-                color: Colors.white,
-                child: ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(24)),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      cardInfo?.number.toString() == null
-                          ? Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Lottie.asset(
-                                  'assets/loading_animation.json',
-                                  width: MediaQuery.of(context).size.width / 2,
-                                  height:
-                                      MediaQuery.of(context).size.height / 8,
-                                ),
-                                Container(
-                                    margin: const EdgeInsets.symmetric(
-                                        horizontal: 24),
-                                    child: const Text(
-                                      "...در حال پردازش",
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold),
-                                    )),
-                              ],
-                            )
-                          : Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Text(
-                                  "شماره کارت \n ${cardInfo?.number.toString()}",
-                                  textAlign: TextAlign.center,
-                                  style: const TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 16),
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    TextButton.icon(
-                                        onPressed: () {
-                                          copyCardInfoToClipboard();
-                                        },
-                                        icon: const Icon(Icons.copy),label:                                     const Text(
-                                      "کپی",
-                                    )
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
+      appBar: buildAppBar(context),
+      body: buildBody(context),
+    );
+  }
+
+  AppBar buildAppBar(BuildContext context) {
+    return AppBar(
+      surfaceTintColor: Theme.of(context).scaffoldBackgroundColor,
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      title: const Text('اسکن کارت اعتباری با هوش مصنوعی'),
+      centerTitle: true,
+    );
+  }
+
+  Widget buildBody(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        buildScannerWidget(),
+        buildBottomContainer(context),
+      ],
+    );
+  }
+
+  ScannerWidget buildScannerWidget() {
+    return ScannerWidget(
+      oneShotScanning: true,
+      cameraResolution: CameraResolution.high,
+      controller: _controller,
+      overlayOrientation: CardOrientation.landscape,
+      overlayText: const Center(
+          child: Text(
+            "کارت بانکی را اسکن کنید",
+            style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white),
+          )),
+    );
+  }
+
+  Align buildBottomContainer(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(24)),
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height / 7,
+          color: Colors.white,
+          child: buildBottomColumn(context),
+        ),
       ),
     );
+  }
+
+  ClipRRect buildBottomColumn(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(24)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: buildBottomChildren(context),
+      ),
+    );
+  }
+
+  List<Widget> buildBottomChildren(BuildContext context) {
+    return cardInfo?.number.toString() == null
+        ? buildLoadingRow(context)
+        : buildCardInfoRow(context);
+  }
+
+  List<Widget> buildLoadingRow(BuildContext context) {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Lottie.asset(
+            'assets/loading_animation.json',
+            width: MediaQuery.of(context).size.width / 2,
+            height: MediaQuery.of(context).size.height / 8,
+          ),
+          Container(
+              margin: const EdgeInsets.symmetric(horizontal: 24),
+              child: const Text(
+                "...در حال پردازش",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              )),
+        ],
+      ),
+    ];
+  }
+
+  List<Widget> buildCardInfoRow(BuildContext context) {
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            "شماره کارت \n ${cardInfo?.number.toString()}",
+            textAlign: TextAlign.center,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton.icon(
+                  onPressed: () {
+                    copyCardInfoToClipboard();
+                  },
+                  icon: const Icon(Icons.copy),
+                  label: const Text(
+                    "کپی",
+                  )),
+            ],
+          ),
+        ],
+      ),
+    ];
   }
 }
